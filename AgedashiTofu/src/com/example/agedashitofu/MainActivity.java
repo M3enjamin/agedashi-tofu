4b27a02b8e7c7d7b85f407d72f630d6f73d8f1cc
M3 <M3enjamin@gmail.com>
2014-04-01 23:25:10 +0200

    Added clickable list item

--- AgedashiTofu/src/com/example/agedashitofu/MainActivity.java
+++ AgedashiTofu/src/com/example/agedashitofu/MainActivity.java
@@ -1,17 +1,42 @@
 package com.example.agedashitofu;
 
+import java.util.List;
+
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
+import android.widget.ListView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends Activity {
+
+	private static String techcrunchFeed = "http://feeds.feedburner.com/TechCrunch?fmt=xml";
+	private RequestQueue queue;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		queue = Volley.newRequestQueue(this);
+		XmlRequest<List<TCNews>> tcRequest = new XmlRequest<List<TCNews>>(
+				techcrunchFeed, new TCFeedParser<List<TCNews>>(),
+				new Response.Listener<List<TCNews>>() {
+					@Override
+					public void onResponse(List<TCNews> response) {
+						displayFeed(response);
+					}
+				}, new Response.ErrorListener() {
+					@Override
+					public void onErrorResponse(VolleyError error) {
+						throw new RuntimeException(error);
+					}
+				});
+		queue.add(tcRequest);
+
 	}
 
 	@Override
@@ -21,14 +46,9 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-	public void goToContacts(View view) {
-		Intent intent = new Intent(this, ContactListLoader.class);
-		startActivity(intent);
-	}
-
-	public void goToCustomList(View view) {
-		Intent intent = new Intent(this, CustomList.class);
-		startActivity(intent);
+	public void displayFeed(List<TCNews> news) {
+		ListView listView = (ListView) findViewById(R.id.custom_list);
+		listView.setAdapter(new CustomListAdapter(this, queue, news));
 	}
 
 }
